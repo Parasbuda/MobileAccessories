17573b348b7716e9ded0e328e4536bca464ecdb2
Paras Buda <parasbuda1234@gmail.com>
2020-07-16 16:51:33 +0545

    1st level Routing is done

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import {Route,Switch} from "react-router-dom"
 
 import Header from "./Component/header/Header"
 import About from "./Component/Pages/About/About"
 import Contact from "./Component/Pages/Contact/Contact"
 import Blog from "./Component/Pages/Blog/Blog"
-function App() {
+class App extends React.Component {
+  render(){
   return (
     <div >
     <Header/>
-     <About/>
-     <Contact/>
-     <Blog/>
+    <Switch>
+  <Route exact path="/" component={()=><div>Hlw from Home</div>}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
+      <Route path="/blog" component={Blog}/>
+      </Switch>
     </div>
   );
+}
 }
 
 export default App;
